59c8b115d46d710ee56ebc1eedb1b269379b14b2
jeremyagabriel <jeremyagabriel@gmail.com>
2021-02-03 21:47:32 -0800

    fix

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
-import Preview from 'src/assets/images/linkpreview.jpg';
+import Preview from '../../../static/linkpreview.jpg';
 
 export const SEO = () => {
 
