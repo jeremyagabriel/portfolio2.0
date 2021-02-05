256eb93b6c579ddbadd13380771ade9a4430c94f
jeremyagabriel <jeremyagabriel@gmail.com>
2021-02-04 22:13:57 -0800

    again

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx, Box, Container } from 'theme-ui';
+import React from 'react';
 import { RecoilRoot } from 'recoil';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
@@ -8,24 +9,26 @@ import { SEO } from '../components/SEO';
 
 export const Main = ({ children }) => {
   return (
-    <RecoilRoot>
+    <>
       <SEO />
-      <Container
-        data-comp={Main.displayName}
-        display='flex'
-        flexDirection='column'
-        height='100vh'
-        overflow='hidden'
-      >
-        <Header />
+      <RecoilRoot>
+        <Container
+          data-comp={Main.displayName}
+          display='flex'
+          flexDirection='column'
+          height='100vh'
+          overflow='hidden'
+        >
+          <Header />
 
-        <Box flex={1}>
-          { children }
-        </Box>
+          <Box flex={1}>
+            { children }
+          </Box>
 
-        <Footer />
-      </Container>
-    </RecoilRoot>
+          <Footer />
+        </Container>
+      </RecoilRoot>
+    </>
   )
 }
 
